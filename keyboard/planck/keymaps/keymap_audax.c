55d6de83c45810464d5655ab9a2b9577581c17f2
Jens Kadenbach <gitdax@daxbau.net>
2016-02-28 15:08:44 +0100

    Removed neo layer 4

--- keyboard/planck/keymaps/keymap_audax.c
+++ keyboard/planck/keymaps/keymap_audax.c
@@ -21,21 +21,28 @@
 // entirely and just use numbers.
 #define _NE 0
 #define _RS 1
+#define _LW 2
 
 #define NEO_L1_Y 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NE] = { /* Neo 2 Soft */
-  {MEH_T(KC_TAB),NEO_X,   NEO_V,           NEO_L,   NEO_C,   NEO_W,        NEO_K,        NEO_H,   NEO_G,   NEO_F,   NEO_Q,   KC_BSPC},
-  {NEO_L1_L,     NEO_U,   NEO_I,           NEO_A,   NEO_E,   NEO_O,        NEO_S,        NEO_N,   NEO_R,   NEO_T,   NEO_D,   F(1)},
-  {KC_LSFT,      NEO_UE,  NEO_OE,          NEO_AE,  NEO_P,   NEO_Z,        NEO_B,        NEO_M,   KC_COMM, KC_DOT,  NEO_J,   SFT_T(KC_ESC)},
-  {KC_LCTL,      KC_LALT, KC_LSFT|KC_LGUI, KC_ESC,  NEO_L1_R,GUI_T(KC_SPC),CTL_T(KC_ENT),NEO_L2_L,F(0),    KC_PGUP, KC_PGDN, S(KC_INS)}
+  {KC_TAB,       NEO_X,   NEO_V,            NEO_L,    NEO_C,   NEO_W,      NEO_K,          NEO_H,   NEO_G,   NEO_F,   NEO_Q,   KC_BSPC},
+  {NEO_L1_L,     NEO_U,   NEO_I,            NEO_A,    NEO_E,   NEO_O,      NEO_S,          NEO_N,   NEO_R,   NEO_T,   NEO_D,   F(1)},
+  {KC_LSFT,      NEO_UE,  NEO_OE,           NEO_AE,   NEO_P,   NEO_Z,      NEO_B,          NEO_M,   KC_COMM, KC_DOT,  NEO_J,   SFT_T(KC_ESC)},
+  {KC_LCTL,      KC_LALT, KC_LSFT|KC_LGUI,  MO(_RS),  KC_LCTL, KC_SPC,     LT(_LW, KC_ENT),KC_LCTL, F(0),    KC_PGUP, KC_PGDN, S(KC_INS)}
 },
 [_RS] = { /* RAISE */
-  {KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,         KC_7,    KC_8,    KC_9,    KC_0,    NEO_SS},
-  {KC_TRNS,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,        KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {KC_TRNS,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,       KC_F12,       DF(_NE), KC_TRNS, DF(_RS), RESET,   KC_TRNS},
-  {KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,      KC_7,           KC_8,    KC_9,    KC_0,    NEO_SS},
+  {KC_TRNS,      KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,     KC_MINS,        KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+  {KC_TRNS,      KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,    DF(_NE),        KC_TRNS, DF(_RS), RESET,   KC_TRNS},
+  {KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,        KC_TRNS, KC_VOLD, KC_VOLU, KC_MPLY}
+},
+[_LW] = { /* LOWER */
+  {KC_TRNS,     LCTL(NEO_X),KC_DEL, KC_UP,  LCTL(NEO_C),KC_TRNS,  KC_0,     KC_1,            KC_2,     KC_3,  KC_TRNS,  KC_TRNS},
+  {KC_TRNS,     KC_HOME,  KC_LEFT,  KC_DOWN,KC_RIGHT, KC_END,   KC_GRV,   KC_4,            KC_5,     KC_6,  KC_TRNS,  KC_TRNS},
+  {KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,  KC_PGDN,  KC_TRNS,  KC_7,            KC_8,     KC_9,  KC_TRNS,  KC_TRNS},
+  {KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS}
 }
 };
 
