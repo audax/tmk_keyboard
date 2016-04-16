8effc8315af91457c39f41d04075e9932963614a
audax <gitdax@daxbau.net>
2016-04-16 08:37:28 +0000

    no neo_l1 on neo_y

--- keyboard/planck/keymaps/audax.c
+++ keyboard/planck/keymaps/audax.c
@@ -24,25 +24,26 @@
 #define _LW 2
 
 #define NEO_L1_Y 1
+#define M_AT 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NE] = { /* Neo 2 Soft */
   {KC_TAB,       NEO_X,   NEO_V,            NEO_L,    NEO_C,   NEO_W,      NEO_K,          NEO_H,   NEO_G,   NEO_F,   NEO_Q,   KC_BSPC},
-  {NEO_L1_L,     NEO_U,   NEO_I,            NEO_A,    NEO_E,   NEO_O,      NEO_S,          NEO_N,   NEO_R,   NEO_T,   NEO_D,   F(1)},
+  {NEO_L1_L,     NEO_U,   NEO_I,            NEO_A,    NEO_E,   NEO_O,      NEO_S,          NEO_N,   NEO_R,   NEO_T,   NEO_D,   NEO_Y},
   {KC_LSFT,      NEO_UE,  NEO_OE,           NEO_AE,   NEO_P,   NEO_Z,      NEO_B,          NEO_M,   KC_COMM, KC_DOT,  NEO_J,   SFT_T(KC_ESC)},
-  {KC_LGUI,      KC_LALT, KC_LSFT|KC_LGUI,  MO(_RS),  KC_LCTL, KC_SPC,     LT(_LW, KC_ENT),KC_LCTL, F(0),    KC_PGUP, KC_PGDN, S(KC_INS)}
+  {KC_LGUI,      KC_LALT, S(KC_LGUI),       MO(_RS),  KC_LCTL, KC_SPC,     LT(_LW, KC_ENT),KC_LCTL, F(0),    KC_PGUP, KC_PGDN, S(KC_INS)}
 },
 [_RS] = { /* RAISE */
   {KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,      KC_7,           KC_8,    KC_9,    KC_0,    KC_TRNS},
-  {KC_TRNS,      KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,     KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, M(1)},
+  {KC_TRNS,      KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,     KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, M(M_AT)},
   {KC_TRNS,      KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,    DF(_NE),        KC_TRNS, DF(_RS), RESET,   KC_TRNS},
   {KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,        KC_TRNS, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 [_LW] = { /* LOWER */
   {KC_TRNS,     KC_PGUP,  KC_BSPC,  KC_UP,  KC_DEL,   KC_PGDN,  KC_0,     KC_1,            KC_2,     KC_3,  NEO_SS,  KC_DEL},
-  {KC_TRNS,     KC_HOME,  KC_LEFT,  KC_DOWN,KC_RIGHT, KC_END,   KC_GRV,   KC_4,            KC_5,     KC_6,  LALT(KC_TAB),  M(1)},
+  {KC_TRNS,     KC_HOME,  KC_LEFT,  KC_DOWN,KC_RIGHT, KC_END,   KC_GRV,   KC_4,            KC_5,     KC_6,  LALT(KC_TAB),  M(M_AT)},
   {KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,  KC_PGDN,  KC_TRNS,  KC_7,            KC_8,     KC_9,  LGUI(S(NEO_Q)),  KC_TRNS},
-  {KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS}
+  {KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  S(KC_INS)}
 }
 };
 
@@ -64,7 +65,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
             unregister_code(KC_RSFT);
           }
         break;
-        case 1:
+      case M_AT: // @
           if (record->event.pressed) {
             register_code(NEO_L1_L);
 	    return MACRO(DOWN(NEO_Y), END);
@@ -73,8 +74,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 	    return MACRO(UP(NEO_Y), END);
           }
         break;
-	return MACRO_NONE;
       }
+      return MACRO_NONE;
 };
 
 
