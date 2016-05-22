e3d38d6b78d579f9536979a0f9441ab248184ac8
audax <gitdax@daxbau.net>
2016-05-22 12:43:16 +0000

    additional NEO_SS

--- keyboard/ergodox_ez/keymaps/audax/keymap.c
+++ keyboard/ergodox_ez/keymaps/audax/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_DEL, KC_TRNS,
     // right hand
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_TRNS,
-       KC_TRNS, KC_0, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_0, KC_1, KC_2, KC_3, KC_TRNS, NEO_SS,
                 KC_COMM, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_DOT, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
